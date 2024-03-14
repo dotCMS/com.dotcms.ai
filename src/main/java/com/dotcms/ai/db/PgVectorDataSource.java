9cdbb8f549a7aa88acf41b268b81e3d893829f86
Will Ezell <will@dotcms.com>
2024-03-14 16:58:35 -0400

    feedback

--- src/main/java/com/dotcms/ai/db/PgVectorDataSource.java
+++ src/main/java/com/dotcms/ai/db/PgVectorDataSource.java
@@ -6,16 +6,25 @@ import com.dotmarketing.util.UtilMethods;
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 import io.vavr.Lazy;
+
 import javax.sql.DataSource;
 
-
+/**
+ * This class will return the Datasource that is configured for the ai plugin, or will return the default datasource if no specific DB is configured for AI
+ */
 class PgVectorDataSource {
 
-
+    /**
+     * returns the configured datasource
+     */
     public static final Lazy<DataSource> datasource = Lazy.of(() -> resolveDataSource());
 
 
-
+    /**
+     * Checks if a specific AI datasource is provided, if not, returns the standard dotCMS datasource
+     *
+     * @return
+     */
     private static DataSource resolveDataSource() {
         if (UtilMethods.isEmpty(Config.getStringProperty("AI_DB_BASE_URL", null))) {
             return DbConnectionFactory.getDataSource();
@@ -25,6 +34,11 @@ class PgVectorDataSource {
 
     }
 
+    /**
+     * Builds the ai specific datasource.
+     *
+     * @return
+     */
     private static DataSource internalDatasource() {
         String userName = Config.getStringProperty("AI_DB_USERNAME");
         String password = Config.getStringProperty("AI_DB_PASSWORD");
